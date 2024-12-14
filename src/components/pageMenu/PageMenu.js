0a395bd3d910f64b6a749a5d0a7945ae7a220930
nguyenlongtruong <nguyenlongtruong2003@gmail.com>
2024-12-14 20:55:24 +0700

    small change page manu

--- src/components/pageMenu/PageMenu.js
+++ src/components/pageMenu/PageMenu.js
@@ -1,25 +1,26 @@
-import React from 'react'
-import './PageMenu.scss'
-import { NavLink } from 'react-router-dom'
+import React from "react";
+import "./PageMenu.scss"
+import { NavLink } from "react-router-dom";
+
 
 const PageMenu = () => {
-  return (
-    <div>
-      <nav className='--bg-primary --p --mb'>
-        <ul className='home-links'>
-          <li>
-            <NavLink to='/profile'>Hồ sơ</NavLink>
-          </li>
-          <li>
-            <NavLink to='/wallet'>Ví</NavLink>
-          </li>
-          <li>
-            <NavLink to='/wishlist'>Mua sau</NavLink>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  )
+    return (
+        <div>
+            <nav className="--bg-primary --p --mb">
+                <ul className="home-links">
+                    <li>
+                        <NavLink to="/profile">Trang cá nhân</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/wallet">Ví</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/wishlist">Mua sau</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
-export default PageMenu
+export default PageMenu;
